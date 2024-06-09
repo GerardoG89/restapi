bad28e4baf2189f0bf31f1c62b2413e5e7d2d988
Gerardo Gomez <ge.gomez03@gmail.com>
2024-06-09 16:49:31 -0600

    first commit

--- server.js
+++ server.js
@@ -34,6 +34,85 @@ app.get('/api/resta/:n1/:n2',(req,res)=>{
     res.send ('La resta es: ' + resta);
 });
 
+app.post('/api/nuevousuario', (req,res)=>{
+    let cuerpoRequest = req.body;
+    
+    let nombre = cuerpoRequest.nombre;
+    let apellido = cuerpoRequest.apellido;
+    let telefono = cuerpoRequest.telefono;
+
+    let respuesta = "El nombre es:" + nombre + "El apellido es: "+ apellido + "y el telefono es:" + telefono;
+
+    res.send(respuesta);
+
+});
+
+app.post('/api/nuevasuma', (req,res)=>{
+    let bodyRequest = req.body;
+    
+    let numero1 = bodyRequest.numero1;
+    let numero2 = bodyRequest.numero2;
+
+    let suma = (numero1) + (numero2);
+
+    let resultado ="La suma es: " + suma;
+
+    res.send(resultado);
+
+});
+
+app.put('/api/actualizarusuario/',(req,res)=>{
+    let cuerpoRequest =  req.body
+    res.send ("Usuario actualizado");
+
+});
+
+app.delete('api/eliminarusuario/',(req,res)=>{
+    let cuerpoRequest = req.body
+    res.send("Usuario eliminado")
+});
+
+//////////////////////////////////////////////////////////////////
+//CRUD (CRATE, READ,UPDATE,DELETE)
+
+let items =[];
+let id = 1;
+
+app.get('/api/traeritems',(req,res)=>{
+    res.json(items);
+});
+
+app.post('/appi/agregaritems',(req,res)=>{
+    let cuerpoRequest = req.body
+    const newitem={
+        id:id++,
+        descripcion:cuerpoRequest.descripcion,
+        precio:cuerpoRequest.precio
+    }
+     items.push(newitem);
+     res.json(newitem);
+   
+})
+
+app.put('/api/actualizaritem/:idenvio',(req,res)=>{
+    let id =req.params.id;
+    let cuerpoRequest = req.body;
+    let item= items.find((item)=>item.id==idenvio);
+    if (item){
+        item.descripcion = cuerpoRequest.descripcion
+        item.precio =cuerpoRequest.precio;
+        res.json(item);
+    }else{
+        res.status(404).send ('Item no encontrado');
+    }
+})
+
+app.delete('/api/eliminaritem/:idenvio', (req,res)=>{
+    let idenvio = req.params.idenvio;
+    items = items.filter((item)=>item.id != idenvio);
+    res.json({mensaje: 'item eliminado'});
+});
+
 app.listen(puerto,()=>{
     console.log('servidor escuchando en el puerto' + puerto);
 
